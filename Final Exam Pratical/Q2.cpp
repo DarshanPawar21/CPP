d254922398c40b0bea400b013c85d40ca838341f
DARSHAN PAWAR <dp7878038@gmail.com>
2026-09-16 18:07:26 +0530

    Add files via upload
    .../

---
+++ Final Exam Pratical/Q2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+class Person
+{
+    string name;
+    int age;
+    string address;
+public:
+    Person(string n, int a, string add)
+    {
+	this ->name = n;
+    this-> age = a;
+    this-> address = add;
+    }
+
+    void getdata()
+    {
+        cout << "Name : " << this->name << endl;
+        cout << "Age : " << this->age << endl;
+        cout << "Address : " << this-> address << endl << endl;
+    }
+};
+int main()
+{
+    Person p1("Darshan", 20, "Ahmedabad");
+    Person p2("Rahul", 21, "Surat");
+
+    p1.getdata();
+    p2.getdata();
+
+    return 0;
+}
---
+++ Final Exam Pratical/q4.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<conio.h>	
+using namespace std;
+class Animal
+{
+public:
+    virtual void displayDetails()
+    {
+        cout << "This is an Animal" << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void displayDetails()
+    {
+        cout << "Dog Sound: Bark" << endl;
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    void displayDetails()
+    {
+        cout << "Cat Sound: Meow" << endl;
+    }
+};
+int main()
+{
+    Dog d;
+    Cat c;
+    Animal *a[2];
+
+    a[0] = &d;
+    a[1] = &c;
+
+    a[0]->displayDetails();
+    a[1]->displayDetails();
+
+    return 0;
+}
